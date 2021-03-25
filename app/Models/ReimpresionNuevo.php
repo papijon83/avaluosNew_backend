cd08ea8959e94a60fccc4baac7cb9b8eb6d6c3b5
jpalacig <papijon83@gmail.com>
2021-03-25 15:56:59 -0600

    Modificacion Delegacion

--- app/Models/ReimpresionNuevo.php
+++ app/Models/ReimpresionNuevo.php
@@ -2573,7 +2573,7 @@ class ReimpresionNuevo
         $infoReimpresion['Ubicacion_Inmueble']['No_Interior'] = $ubicacionInmueble['NumeroInterior'];
         $infoReimpresion['Ubicacion_Inmueble']['Colonia'] = $ubicacionInmueble['Colonia'];
         $infoReimpresion['Ubicacion_Inmueble']['CP'] = $ubicacionInmueble['CodigoPostal'];
-        $infoReimpresion['Ubicacion_Inmueble']['Delegacion'] = isset($ubicacionInmueble['Delegacion']) ? $ubicacionInmueble['Delegacion'] : $ubicacionInmueble['Alcaldia'];
+        $infoReimpresion['Ubicacion_Inmueble']['Delegacion'] = isset($ubicacionInmueble['Delegacion']) ? $this->modelDocumentos->ObtenerNombreDelegacionPorClave($ubicacionInmueble['Delegacion']) : $this->modelDocumentos->ObtenerNombreDelegacionPorClave($ubicacionInmueble['Alcaldia']);
         $infoReimpresion['Ubicacion_Inmueble']['Edificio'] = "-";
         $infoReimpresion['Ubicacion_Inmueble']['Lote'] = isset($ubicacionInmueble['Lote']) && !is_array($ubicacionInmueble['Lote']) ? $ubicacionInmueble['Lote'] : 0;
         $infoReimpresion['Ubicacion_Inmueble']['Cuenta_agua'] = isset($ubicacionInmueble['CuentaDeAgua']) && !is_array($ubicacionInmueble['CuentaDeAgua']) ? $ubicacionInmueble['CuentaDeAgua'] : '';
