92d6dd7bdf4d5d82755a6fb350656492fc8f3f49
jpalacig <papijon83@gmail.com>
2021-03-30 17:26:14 -0600

    Modificaciones Pruebas

--- app/Http/Controllers/ClienteWSController.php
+++ app/Http/Controllers/ClienteWSController.php
@@ -59,4 +59,35 @@ class ClienteWSController extends Controller
             return response()->json($response->getBody(), $response->getStatusCode());
         }
     }
+
+    public function token($folio)
+    {
+
+        $usuario = "U4566";
+        $auth = '<usuario xmlns="IDEAS.Avametrica">' . env('USUARIO_WSDL') . '</usuario>';
+        $auth .= '<contrasenia xmlns="IDEAS.Avametrica">' . env('PASS_WSDL') . '</contrasenia>';
+        //$file = storage_path('app/24042020_PRUEBA SAF.xml');
+        //$xml = simplexml_load_file($file);
+        $client = new \SoapClient(env('WSDL_RECIBE_AVALUO'), ["debug" => true, "trace" => true, "exception" => true]);
+        $auth_block = new \SoapVar($auth, XSD_ANYXML, NULL, NULL, NULL, NULL);
+        $header = new \SoapHeader('http://schemas.xmlsoap.org/soap/envelope/', 'Header', $auth_block);
+        $client->__setSoapHeaders($header);
+        try {
+            $client->__soapCall('obtenertoken', ['obtenertoken' => ['folio_avaluo' => $folio]]);
+        } catch (\Exception $e) {
+        }
+        $restClient = new \GuzzleHttp\Client();
+        $response = $restClient->request('POST', env('API_WEBHOOK') . $folio);
+
+        if ($response->getStatusCode() == 200) {
+            try {
+                echo "ENTREEEEEE"; exit();
+                
+            } catch (\Exception $e) {
+                return response()->json(['mensaje' => $e], 500);
+            }
+        } else {
+            return response()->json($response->getBody(), $response->getStatusCode());
+        }
+    }
 }
