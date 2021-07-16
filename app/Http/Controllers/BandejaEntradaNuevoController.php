66ec54508bdd61d4234e7a8cebc0c5de3a015b3a
mdperezr13 <mdperezr13@gmail.com>
2021-07-16 16:13:29 -0500

    error log

--- app/Http/Controllers/BandejaEntradaNuevoController.php
+++ app/Http/Controllers/BandejaEntradaNuevoController.php
@@ -6829,7 +6829,7 @@ class BandejaEntradaNuevoController extends Controller
                 } else {
                     shell_exec('rm '. storage_path('app/*.docx'));
                     shell_exec('pdf2docx convert '.storage_path('app/formato.pdf').' '.storage_path('app/'.$numero_unico.'.docx'));
-                    Log::info(storage_path('app/*.docx'));
+                    error_log(storage_path('app/'.$numero_unico.'.docx'));
                     return response()->json(['docxbase64' => base64_encode(Storage::get($numero_unico.'.docx')), 'nombre' =>  $numero_unico . '.docx'], 200);
                 }     
             /*$this->modelReimpresion = new ReimpresionNuevo();
@@ -6862,9 +6862,8 @@ class BandejaEntradaNuevoController extends Controller
                     return response()->json(['pdfbase64' => base64_encode(Storage::get('formato.pdf')), 'nombre' =>  $numero_unico . '.pdf'], 200);
                 } else {
                     shell_exec('rm '. storage_path('app/*.doc'));
-                    
                     shell_exec('pdf2docx convert '.storage_path('app/formato.pdf').' '.storage_path('app/'.$numero_unico.'.docx'));
-                    Log::info(storage_path('app/*.docx'));
+                    error_log(storage_path('app/'.$numero_unico.'.docx'));
                     return response()->json(['docxbase64' => base64_encode(Storage::get($numero_unico.'.docx')), 'nombre' =>  $numero_unico . '.docx'], 200);
                 } 
                 /*$this->modelDocumentos = new Documentos();    //echo $numero_unico; exit();         
