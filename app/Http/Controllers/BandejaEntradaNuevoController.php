630a723228f9a34d1e880e32e23db2d2f7fd03e7
mdperezr13 <mdperezr13@gmail.com>
2021-07-19 14:57:30 -0500

    test

--- app/Http/Controllers/BandejaEntradaNuevoController.php
+++ app/Http/Controllers/BandejaEntradaNuevoController.php
@@ -6829,8 +6829,8 @@ class BandejaEntradaNuevoController extends Controller
                 } else {
                     //Log::info('python3 pdf2doc.py '.storage_path('app/formato.pdf').' '.storage_path('app/formato.docx'));
                     shell_exec('rm '. storage_path('app/*.docx'));
-                    $output = shell_exec('python3 pdf2doc.py '.storage_path('app/formato.pdf').' '.storage_path('app/formato.docx'));
-                    Log::info($output);
+                    $python = 'python3 pdf2doc.py '.storage_path('app/formato.pdf').' '.storage_path('app/formato.docx');
+                    echo $python;
                     //shell_exec('pdf2docx convert '.storage_path('app/formato.pdf').' '.storage_path('app/formato.doc'));
                     //return response()->json(['docxbase64' => base64_encode(Storage::get('formato.docx')), 'nombre' =>  $numero_unico . '.docx'], 200);
                 }     
@@ -6864,8 +6864,8 @@ class BandejaEntradaNuevoController extends Controller
                     return response()->json(['pdfbase64' => base64_encode(Storage::get('formato.pdf')), 'nombre' =>  $numero_unico . '.pdf'], 200);
                 } else {
                     shell_exec('rm '. storage_path('app/*.docx'));
-                    $output = shell_exec('python3 pdf2doc.py '.storage_path('app/formato.pdf').' '.storage_path('app/formato.docx'));
-                    Log::info($output);
+                    $python = 'python3 pdf2doc.py '.storage_path('app/formato.pdf').' '.storage_path('app/formato.docx');
+                    echo $python;
                 } 
                 /*$this->modelDocumentos = new Documentos();    //echo $numero_unico; exit();         
             $id_avaluo = $this->modelDocumentos->get_idavaluo_db($numero_unico);    
