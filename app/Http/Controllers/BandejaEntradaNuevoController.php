cdde7bf6c05c9f1bf0ceca61b16dc20d132cff91
mdperezr13 <mdperezr13@gmail.com>
2021-07-19 11:04:01 -0500

    test

--- app/Http/Controllers/BandejaEntradaNuevoController.php
+++ app/Http/Controllers/BandejaEntradaNuevoController.php
@@ -6827,9 +6827,16 @@ class BandejaEntradaNuevoController extends Controller
                 if($format == 'PDF'){
                     return response()->json(['pdfbase64' => base64_encode(Storage::get('formato.pdf')), 'nombre' =>  $numero_unico . '.pdf'], 200);
                 } else {
+                    shell_exec('rm '. storage_path('app/*.doc'));
+                    $fileName = 'formato';
+                    $pathpdf = storage_path('app/' . $fileName . '.pdf');
+                    $pathdoc = storage_path('app/' . $fileName . '.doc');
+                    file_put_contents($pathpdf, $pdf->output());
+                    shell_exec('libreoffice --headless --infilter="writer_pdf_import" --convert-to doc  --outdir '. storage_path('app/') . ' ' . $pathpdf);
+                    shell_exec('rm ' . $pathpdf);
                     //Log::info('python3 pdf2doc.py '.storage_path('app/formato.pdf').' '.storage_path('app/formato.docx'));
-                    shell_exec('rm '. storage_path('app/*.docx'));
-                    shell_exec('python3 pdf2doc.py '.storage_path('app/formato.pdf').' '.'formato.docx');
+                    //shell_exec('rm '. storage_path('app/*.docx'));
+                    //shell_exec('python3 pdf2doc.py '.storage_path('app/formato.pdf').' '.'formato.docx');
                     //shell_exec('pdf2docx convert '.storage_path('app/formato.pdf').' '.storage_path('app/formato.doc'));
                     //return response()->json(['docxbase64' => base64_encode(Storage::get('formato.docx')), 'nombre' =>  $numero_unico . '.docx'], 200);
                 }     
@@ -6862,9 +6869,16 @@ class BandejaEntradaNuevoController extends Controller
                 if($format == 'PDF'){
                     return response()->json(['pdfbase64' => base64_encode(Storage::get('formato.pdf')), 'nombre' =>  $numero_unico . '.pdf'], 200);
                 } else {
+                    shell_exec('rm '. storage_path('app/*.doc'));
+                    $fileName = 'formato';
+                    $pathpdf = storage_path('app/' . $fileName . '.pdf');
+                    $pathdoc = storage_path('app/' . $fileName . '.doc');
+                    file_put_contents($pathpdf, $pdf->output());
+                    shell_exec('libreoffice --headless --infilter="writer_pdf_import" --convert-to doc  --outdir '. storage_path('app/') . ' ' . $pathpdf);
+                    shell_exec('rm ' . $pathpdf);
                     //Log::info('python3 pdf2doc.py '.storage_path('app/formato.pdf').' '.storage_path('app/formato.docx'));
-                    shell_exec('rm '. storage_path('app/*.docx'));
-                    shell_exec('python3 pdf2doc.py '.storage_path('app/formato.pdf').' '.'formato.docx');
+                    //shell_exec('rm '. storage_path('app/*.docx'));
+                    //shell_exec('python3 pdf2doc.py '.storage_path('app/formato.pdf').' '.'formato.docx');
                 } 
                 /*$this->modelDocumentos = new Documentos();    //echo $numero_unico; exit();         
             $id_avaluo = $this->modelDocumentos->get_idavaluo_db($numero_unico);    
