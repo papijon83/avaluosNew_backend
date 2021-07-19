33676f5f4d6ef731e7039427b7eb44163ba64efd
mdperezr13 <mdperezr13@gmail.com>
2021-07-19 16:14:47 -0500

    test

--- app/Http/Controllers/BandejaEntradaNuevoController.php
+++ app/Http/Controllers/BandejaEntradaNuevoController.php
@@ -6830,8 +6830,7 @@ class BandejaEntradaNuevoController extends Controller
                     //Log::info('python3 pdf2doc.py '.storage_path('app/formato.pdf').' '.storage_path('app/formato.docx'));
                     shell_exec('rm '. storage_path('app/*.docx'));
                     $command = 'python3 pdf2doc.py '.storage_path('app/formato.pdf').' '.storage_path('app/formato.docx');
-                    Log::info($command);
-                    //shell_exec($command);
+                    shell_exec(escapeshellcmd(escapeshellarg($command)));
                     //shell_exec('pdf2docx convert '.storage_path('app/formato.pdf').' '.storage_path('app/formato.doc'));
                     //return response()->json(['docxbase64' => base64_encode(Storage::get('formato.docx')), 'nombre' =>  $numero_unico . '.docx'], 200);
                 }     
@@ -6866,7 +6865,7 @@ class BandejaEntradaNuevoController extends Controller
                 } else {
                     shell_exec('rm '. storage_path('app/*.docx'));
                     $command = 'python3 pdf2doc.py '.storage_path('app/formato.pdf').' '.storage_path('app/formato.docx');
-                    Log::info($command);
+                    shell_exec(escapeshellcmd(escapeshellarg($command)));
                     //shell_exec($command);
                 } 
                 /*$this->modelDocumentos = new Documentos();    //echo $numero_unico; exit();         
