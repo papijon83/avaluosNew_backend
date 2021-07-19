3794311bf76d811e8fbed9e3e0d6e500878ee484
mdperezr13 <mdperezr13@gmail.com>
2021-07-19 00:29:55 -0500

    test

--- public/pdf2doc.py
+++ public/pdf2doc.py
@@ -8,5 +8,5 @@ docx_file = sys.argv[2]
 
 # convert pdf to docx
 cv = Converter(pdf_file)
-cv.convert(docx_file)      # all pages by default
+cv.convert(docx_file, multi_processing=True)      # all pages by default
 cv.close()
