76b04a27c41da083f5f9a5b9edfb72e8bf3c8beb
mdperezr13 <mdperezr13@gmail.com>
2021-07-20 06:51:23 -0500

    test

--- public/pdf2doc.py
+++ public/pdf2doc.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
 import sys
-from pdf2docx import Converter
+from pdf2docx import parse
 
 pdf_file = sys.argv[1]
 docx_file = sys.argv[2]
 
 # convert pdf to docx
-cv = Converter(pdf_file)
-cv.convert(docx_file)      # all pages by default
-cv.close()
+parse(pdf_file, docx_file)
