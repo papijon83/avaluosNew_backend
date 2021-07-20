46b1c976e4fb7ceda323b2331b80ef919eaa3e1c
mdperezr13 <mdperezr13@gmail.com>
2021-07-19 19:01:37 -0500

    test

--- app/Http/Controllers/BandejaEntradaNuevoController.php
+++ app/Http/Controllers/BandejaEntradaNuevoController.php
@@ -6834,7 +6834,7 @@ class BandejaEntradaNuevoController extends Controller
                     file_put_contents($pathpdf, $pdf->output());
                     shell_exec('libreoffice --headless --infilter="writer_pdf_import" --convert-to doc  --outdir '. storage_path('app/') . ' ' . $pathpdf);
                     shell_exec('rm ' . $pathpdf);
-                    return response()->json(['docxbase64' => base64_encode(Storage::get('formato.doc')), 'nombre' =>  $numero_unico . '.doc'], 200);
+                    return response()->json(['docxbase64' => base64_encode($pathdoc), 'nombre' =>  $numero_unico . '.doc'], 200);
                 }     
             /*$this->modelReimpresion = new ReimpresionNuevo();
             $infoAvaluo = $this->modelReimpresion->infoAvaluo($id_avaluo);
@@ -6872,7 +6872,7 @@ class BandejaEntradaNuevoController extends Controller
                     file_put_contents($pathpdf, $pdf->output());
                     shell_exec('libreoffice --headless --infilter="writer_pdf_import" --convert-to doc  --outdir '. storage_path('app/') . ' ' . $pathpdf);
                     shell_exec('rm ' . $pathpdf);
-                    return response()->json(['docxbase64' => base64_encode(Storage::get('formato.doc')), 'nombre' =>  $numero_unico . '.doc'], 200);
+                    return response()->json(['docxbase64' => base64_encode($pathdoc), 'nombre' =>  $numero_unico . '.doc'], 200);
                 } 
                 /*$this->modelDocumentos = new Documentos();    //echo $numero_unico; exit();         
             $id_avaluo = $this->modelDocumentos->get_idavaluo_db($numero_unico);    
